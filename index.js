96d8716fd98f08ad22946de037324b19091b1917
David Glymph <Woozl@users.noreply.github.com>
2023-10-06 16:16:33 -0400

    fetch from nameres, batch nodes

--- index.js
+++ index.js
@@ -1,66 +1,107 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 import readline from "readline";
 import { cwd } from "process";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const BATCH_SIZE = process.env.BATCH_SIZE ?? 1000;
+const NAME_RESOLVER = process.env.NAME_RESOLVER ?? "https://name-resolution-sri-dev.apps.renci.org";
 
-const [,, inputFileRel, outputDirRel] = process.argv;
-const inputFilePath = path.resolve(cwd(), inputFileRel)
-const outputDirPath = path.resolve(cwd(), outputDirRel)
+const [, , inputFileRel, outputDirRel] = process.argv;
+const inputFilePath = path.resolve(cwd(), inputFileRel);
+const outputDirPath = path.resolve(cwd(), outputDirRel);
 
-const inputSize = fs.statSync(inputFilePath).size
-let bytesReadSoFar = 0;
 const inputFileStream = fs.createReadStream(inputFilePath);
-const rl = readline.createInterface({
+const inputSize = fs.statSync(inputFilePath).size
+const nodelist = readline.createInterface({
   input: inputFileStream,
   crlfDelay: Infinity,
 });
 
 fs.mkdirSync(outputDirPath, { recursive: true });
 const errorFilePath = path.join(outputDirPath, "error.txt");
-if(fs.existsSync(errorFilePath)) fs.unlinkSync(errorFilePath);
-const errorLog = fs.createWriteStream(errorFilePath, { flags: 'a+' });
+if (fs.existsSync(errorFilePath)) fs.unlinkSync(errorFilePath);
+const errorLog = fs.createWriteStream(errorFilePath, { flags: "a+" });
 
 // Map<string, fs.WriteStream>
 const fsMap = new Map();
 
-let t0 = performance.now();
-for await (const line of rl) {
-  bytesReadSoFar += Buffer.byteLength(line) + 1;
-  const t1 = performance.now();
-  if (t1 - t0 > 500) { // every 0.5 second
-    console.log(`${((bytesReadSoFar / inputSize) * 100).toFixed(2)}%`)
-    t0 = performance.now();
-  }
+const sleep = async (delay) =>
+  new Promise((res) => setTimeout(() => res, delay));
+
+const processBatch = async (nodes, batchStartIndex, bytesReadSoFar) => {
+  const t0 = performance.now();
   
-  try {
-    const {
-      id,
-      name,
-      category,
-      // equivalent_identifiers,
-    } = JSON.parse(line);
-    
-    const mainCat = category[0].replace("biolink:", "");
-    let categoryFile = fsMap.get(mainCat);
-    if (categoryFile === undefined) {
-      fsMap.set(mainCat, fs.createWriteStream(path.join(__dirname, '/out/', `${mainCat}.txt`)))
-      categoryFile = fsMap.get(mainCat);
-    }
+  // parse nodes here so we don't have to do it twice
+  const parsedNodes = nodes.map(node => JSON.parse(node));
+  
+  // request node synonyms from nameres for this batch
+  const synonymsList = await fetch(`${NAME_RESOLVER}/reverse_lookup`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      curies: parsedNodes.map(({ id }) => id)
+    })
+  }).then(res => res.json());
+  
+  nodes.forEach((node) => {
+    try {
+      const {
+        id,
+        name,
+        category,
+      } = JSON.parse(node);
+      const synonyms = synonymsList[id];
+      if (!Array.isArray(synonyms)) throw new Error(`Node ${node} did not have synonyms`)
 
-    const outputJson = {
-      curie: id,
-      names: [name],
-      types: category.map((c) => c.replace("biolink:", "")),
-      preferred_name: name,
-      shortest_name_length: name.length,
-    }
+      const mainCat = category[0].replace("biolink:", "");
+      let categoryFile = fsMap.get(mainCat);
+      if (categoryFile === undefined) {
+        fsMap.set(
+          mainCat,
+          fs.createWriteStream(path.join(outputDirPath, `${mainCat}.txt`))
+        );
+        categoryFile = fsMap.get(mainCat);
+      }
 
-    categoryFile.write(`${JSON.stringify(outputJson)}\n`)
+      // use Set to get rid of duplicates
+      const nameList = Array.from(new Set([name, ...synonyms]));
+
+      const outputJson = {
+        curie: id,
+        names: nameList,
+        types: category.map((c) => c.replace("biolink:", "")),
+        preferred_name: name,
+        shortest_name_length: nameList.reduce((smallest, cur) => (
+          cur.length < smallest ? cur.length : smallest
+        ), Infinity),
+      };
+
+      categoryFile.write(`${JSON.stringify(outputJson)}\n`);
+    } catch (e) {
+      errorLog.write(`${node}\n${e}\n\n\n`);
+    }
+  });
+
+  console.log(`[${((bytesReadSoFar / inputSize) * 100).toFixed(2)}%] [${(performance.now() - t0).toFixed(1)}ms] Finished processing batch of nodes ${batchStartIndex} - ${batchStartIndex + nodes.length}`)
+};
+
+let t0 = performance.now();
+const progStart = t0;
+let batch = [];
+let index = 0;
+let bytesReadSoFar = 0;
+for await (const node of nodelist) {
+  bytesReadSoFar += Buffer.byteLength(node) + 1;
+  batch.push(node);
+  if (index % BATCH_SIZE === 0 && index !== 0) {
+    await processBatch(batch, index - BATCH_SIZE, bytesReadSoFar);
+    batch = [];
   }
-  catch (e) {
-    errorLog.write(`${line}\n${e}\n\n\n`)
-  }
+  index += 1;
 }
+await processBatch(batch, index - BATCH_SIZE, bytesReadSoFar); // process remaining
+
+console.log(`Job took ${(performance.now() - progStart / 1000).toFixed(2)} seconds.`)
