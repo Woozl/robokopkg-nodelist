8d780ba65e2e92bd4417caa1460e9723e88eaa9a
David Glymph <Woozl@users.noreply.github.com>
2023-10-09 13:36:52 -0400

    remove bash script

--- check-num-of-lines.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# sum lines of every file in a directory
-
-# `chmod +x check-num-of-lines.sh`
-# `./check-num-of-lines <path to dir>`
-
-total=0
-find $1 -type f -print0 | xargs -0 awk '{ total += length } END { print total }'
